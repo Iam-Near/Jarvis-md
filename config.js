22b83c47d0250b643aeec0886d5ddc1d1032097f
Iam-Near <radhalakshmics8@gmail.com>
2024-09-08 19:41:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
   ANTIBOT_MSG: process.env.ANTIBOT_MSG || "_*Bot Not allowed in this group*_",
   ANTIWORD_MSG: process.env.ANTIWORD_MSG || "_*antiword deleted successfully*_",
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_6bc6_a4f9_a257_ff5b_3d04_ecd3_b548_410c',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_37b6_c478_a141_fc86_c16b_8cc1_c89a_b00d',
   LANG: process.env.LANG || 'EN',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLERS || process.env.HANDLER || process.env.PREFIX || '/',
